4bf9ce3105272f5903ec561431ea3b22151718a2
[Armando Samperio] <armando.samperio@mavs.uta.edu>
2019-10-04 12:36:26 -0500

    add userService

--- server.js
+++ server.js
@@ -7,3 +7,20 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+/*-----Put All Api Routes Right Here------ */
+
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+    console.log(`Express app is running on port ${port}`)
+});

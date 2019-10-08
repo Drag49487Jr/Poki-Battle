5d4cc485f825a50ba4282cff73dae73f56978065
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2019-10-08 10:57:18 -0500

    returns stats but cant render

--- src/components/SearchPokemon/SearchPokemon.jsx
+++ src/components/SearchPokemon/SearchPokemon.jsx
@@ -6,19 +6,14 @@ class SearchPokemon extends Component {
     constructor() {
         super();
         this.state={
-            abilities:[]
+            abilities:[],
         }
     }
-
+    
     async componentDidMount() {
-        axios.get(this.props.searchResult.url)
-        .then(res => 
-            {this.setState({abilities: res.data})
-            const abilities = this.props.searchResult.url
-            console.log(abilities)
-        });
+        axios.get(`${this.props.searchResult.url}`).then(res => this.setState({abilities: res.data.abilities}))
     }
-
+    
     render() {            
             const {region,teamName} = this.props.history.location.state;
         return(
@@ -26,8 +21,8 @@ class SearchPokemon extends Component {
                 <NavBar 
                 user={this.props.user}
                 />
-                {teamName}<br />
-                {region}
+                TeamName: {teamName}<br />
+                Region: {region}
                 <h3>Enter Name</h3>
                 <form onSubmit={this.props.handleSearch}>
                     <input onChange={this.props.handleChange} name="searchTerm" type="text"/>
@@ -35,7 +30,7 @@ class SearchPokemon extends Component {
                 </form>
                 {
                     !this.props.message && this.props.searchResult.name ? 
-                    <p>{this.props.searchResult.name} {this.props.searchResult.url} {console.log(this.state.abilities)}</p>
+                    <p>{this.props.searchResult.name} {axios.get(this.props.searchResult.url).then(res => console.log(res.data))}</p>
                     : <p>{this.props.message} </p>
                 }
             </div>

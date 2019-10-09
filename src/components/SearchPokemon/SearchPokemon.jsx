3947debf4caa5a679921a11eba543254a7f95fe9
[Armando Samperio] <armando.samperio@mavs.uta.edu>
2019-10-08 23:49:52 -0500

    render stats of pokemon success

--- src/components/SearchPokemon/SearchPokemon.jsx
+++ src/components/SearchPokemon/SearchPokemon.jsx
@@ -1,32 +1,47 @@
 import React, {Component} from 'react';
 import NavBar from '../NavBar/NavBar';
 import axios from 'axios';
-const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
 
 class SearchPokemon extends Component {
     constructor() {
         super();
             this.state={
-                abilities: [],
-                stats:[]
+                searchTerm: '',
+                searchResult: '',
+                pokemon: []
             }
     }
     
 
+    handleSearch = e => {
+        e.preventDefault();
+        try {
+            axios.get('https://pokeapi.co/api/v2/pokemon/' + this.state.searchResult)
+            .then(res => 
+                this.setState({
+                    pokemon: res.data,
+                    searchResult: ''
+                })
+            );
+            console.log(this.state.searchResult);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
-    async componentDidMount() {
-        const data = this.props.searchTerm;
-        // event.preventDefault();
-        // console.log(this.state.searchTerm);
-        await axios.get(`${BASE_URL}${data}`).then(res => this.setState({
-            abilities: res.data.abilities,
-            stats: res.data.stats
-        }));
-    }
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
 
     render() {            
-            const {region,teamName} = this.props.history.location.state;
-
+        const {region,teamName} = this.props.history.location.state;
+        const { abilities,base_experience,height,name } = this.state.pokemon;
+        console.log(abilities)
+        console.log(base_experience)
+        console.log(height)
+        console.log(name)
+        console.log(this.state.pokemon);
+        console.log(this.state.searchResult);
         return(
             <div>SearchPokemon
                 <NavBar 
@@ -35,22 +50,10 @@ class SearchPokemon extends Component {
                 TeamName: {teamName}<br />
                 Region: {region}
                 <h3>Enter Name</h3>
-                <form>
-                    <input onChange={this.props.handleChange} name="searchTerm" type="text"/>
-                    <button onClick={this.props.handleSearch} type="submit">Search</button>
-                </form>
-                {
-                    !this.props.message && this.props.searchResult.name ? (
-                        <p>
-                            {this.props.searchResult.name}<br/>
-                            {this.props.searchResult.url} <br/>
-                            {this.state.abilities[0].ability.name} <br/>
-                            {this.state.abilities[1].ability.name}<br/>
-                            {this.state.stats[0].stat.name}
-                        </p>
-                    )
-                    : <p>{this.props.message} </p>
-                }
+                <input onChange={this.handleChange} name='searchResult' type='text' />
+                <button onClick={this.handleSearch}>Search</button>
+                <p>{base_experience}</p>
+                <p>{name}</p>
             </div>
         )
     }

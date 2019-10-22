365827d514cac7f0cb23c4e4acb475592bed360e
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2019-10-22 14:38:23 -0500

    pokemon card is white

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import './PokemonCard.css';
 
 class PokemonCard extends Component {
     
@@ -10,15 +11,19 @@ class PokemonCard extends Component {
 
     render() {
             return(
-                <div>
-                       {this.props.pokemonName ? <button onClick={this.handleRemovePokemon}>X</button>: null}
-                        {this.props.pokemonName ? <p>Name: {this.props.pokemonName}</p> : null}
-                        {this.props.abilities ? <p>{this.props.abilities}</p> : null}
-                        {this.props.weight ? <p>Weight: {this.props.weight}</p> : null}
-                        {this.props.types ? <p>{this.props.types}</p> : null}
-                        {this.props.stats ? <p>{this.props.stats}</p> : null}
-                        {<img src={this.props.sprites}></img>}
-                        {this.props.base_experience ? <p>Base Experience: {this.props.base_experience}</p> : null}
+                <div className='card bg-light'>
+                        {this.props.pokemonName ? <button onClick={this.handleRemovePokemon}>X</button>: null}
+                        {this.props.sprites ? <img className='card-img-top' src={this.props.sprites} /> : null}
+                    <div className='card-body'>
+                        <p className='card-text'>
+                            {this.props.pokemonName ? <p>Name: {this.props.pokemonName.toUpperCase()}</p> : null}
+                            {this.props.abilities ? <p>{this.props.abilities}</p> : null}
+                            {this.props.weight ? <p>Weight: {this.props.weight}</p> : null}
+                            {this.props.types ? <p>{this.props.types}</p> : null}
+                            {this.props.stats ? <p>{this.props.stats}</p> : null}
+                            {this.props.base_experience ? <p>Base Experience: {this.props.base_experience}</p> : null}
+                        </p>
+                    </div>
                 </div>
             )
     }
